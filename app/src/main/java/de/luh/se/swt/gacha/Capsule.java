e3bc8231650acd4a5fbde0ad563c8723eb32d443
Cedric Wellhausen <wellhausen@stud.uni-hannover.de>
2025-11-30 20:37:55 +0100

    feat: Added opening of Capsules

--- app/src/main/java/de/luh/se/swt/gacha/Capsule.java
+++ app/src/main/java/de/luh/se/swt/gacha/Capsule.java
@@ -2,4 +2,14 @@ package de.luh.se.swt.gacha;
 
 public class Capsule {
     
+    private boolean isOpen;
+
+    public Capsule() {
+        this.isOpen = false;
+    }
+
+    public void open() {
+        this.isOpen = true;
+    }
+
 }
