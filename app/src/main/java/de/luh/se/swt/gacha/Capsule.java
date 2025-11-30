e0696ddad2295e9b86c59b29e19885525f82a4ef
Cedric Wellhausen <wellhausen@stud.uni-hannover.de>
2025-11-30 20:38:45 +0100

    fix: Fixed Gacha Capsule not having any Items

--- app/src/main/java/de/luh/se/swt/gacha/Capsule.java
+++ app/src/main/java/de/luh/se/swt/gacha/Capsule.java
@@ -3,13 +3,19 @@ package de.luh.se.swt.gacha;
 public class Capsule {
     
     private boolean isOpen;
+    private Object object;
 
-    public Capsule() {
+    public Capsule(Object o) {
         this.isOpen = false;
+        this.object = o;
     }
 
-    public void open() {
+    public Object open() {
+        if (this.isOpen) {
+            return null;
+        }
         this.isOpen = true;
+        return this.object;
     }
 
 }
