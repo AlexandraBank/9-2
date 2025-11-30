1133f74a9379962dc5637986c67db5e3f06b2f60
Cedric Wellhausen <wellhausen@stud.uni-hannover.de>
2025-11-30 20:39:45 +0100

    feat: GachaPon has Capsules

--- app/src/main/java/de/luh/se/swt/gacha/GachaPon.java
+++ app/src/main/java/de/luh/se/swt/gacha/GachaPon.java
@@ -1,5 +1,11 @@
 package de.luh.se.swt.gacha;
 
 public class GachaPon {
-    
+
+    private Capsule[] capsules;
+
+    public GachaPon(Capsule... capsules) {
+        this.capsules = capsules;
+    }
+
 }
