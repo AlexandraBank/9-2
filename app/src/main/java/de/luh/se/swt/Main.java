99eada03e0b3edbd62ebba2760502d7c64b333f5
Cedric Wellhausen <wellhausen@stud.uni-hannover.de>
2025-11-30 20:46:58 +0100

    ref: Cleanup Main

--- app/src/main/java/de/luh/se/swt/Main.java
+++ app/src/main/java/de/luh/se/swt/Main.java
@@ -5,10 +5,8 @@ import de.luh.se.swt.options.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("HÜ9 Startet...");
 		OptionParser parser = new OptionParser(args, 
-			new Option("--help", true),
-			new Option("--lmao", false)
+			new Option("--help", false)
 		);
 	}
 }
